08abbcbf4f231874697dc326ec738e4741591393
divisor <div@altertech.com>
2019-06-11 02:38:41 +0200

    color fixes

--- src/@altertech/jsaltt/index.js
+++ src/@altertech/jsaltt/index.js
@@ -60,99 +60,96 @@ class Logging {
     this.colored = true;
   }
 
-  debug() {
-    if (this.colored) {
-      var msg = [];
-      Array.from(arguments).map(function(m) {
-        if ((m && m.stack && m.message) || typeof m === 'object') {
-          msg.push(m);
-        } else if (typeof chalk === 'undefined') {
-          msg.push(`%c${m}`);
-          msg.push('font-size: 12px; color: #999');
+  //msg.push('font-size: 12px; color: #999');
+  //msg.push(chalk.gray(m));
+
+  _msg(css, cf) {
+    if (arguments.length > 2) {
+      var out = [].slice.call(arguments, 2);
+      if (this.colored) {
+        var result = [];
+        if (cf) {
+          out.map(function(m) {
+            if ((m && m.stack && m.message) || typeof m === 'object') {
+              result.push(m);
+            } else {
+              result.push(cf(m));
+            }
+          });
         } else {
-          msg.push(chalk.gray(m));
+          var msg;
+          var colorize = css ? true : false;
+          for (var i = 0; i < out.length; i++) {
+            var m = out[i];
+            if (
+              !colorize ||
+              (m && m.stack && m.message) ||
+              typeof m === 'object'
+            ) {
+              if (colorize) {
+                colorize = false;
+                result.push(msg);
+                result.push(css);
+              }
+              result.push(m);
+              msg = null;
+            } else {
+              msg ? (msg += ' ') : (msg = '%c');
+              msg += m;
+            }
+          }
+          if (msg) {
+            result.push(msg);
+            result.push(css);
+          }
         }
-      });
-    } else {
-      var msg = Array.from(arguments);
+        out = result;
+      }
+      console.log.apply(null, out);
     }
-    console.log.apply(null, msg);
+  }
+
+  debug() {
+    var css;
+    var cf;
+    typeof chalk === 'undefined'
+      ? (css = 'font-size: 12px; color: #999')
+      : (cf = chalk.gray);
+    this._msg.apply(this, [css, cf].concat(Array.from(arguments)));
   }
 
   info() {
-    if (this.colored) {
-      var msg = [];
-      Array.from(arguments).map(function(m) {
-        if ((m && m.stack && m.message) || typeof m === 'object') {
-          msg.push(m);
-        } else if (typeof chalk === 'undefined') {
-          msg.push(`%c${m}`);
-          msg.push('font-size: 12px; color: black');
-        } else {
-          msg.push(m);
-        }
-      });
-    } else {
-      var msg = Array.from(arguments);
-    }
-    console.log.apply(null, msg);
+    var css;
+    var cf;
+    typeof chalk === 'undefined' ? (css = 'font-size: 12px; color: black') : {};
+    this._msg.apply(this, [css, cf].concat(Array.from(arguments)));
   }
 
   warning() {
-    if (this.colored) {
-      var msg = [];
-      Array.from(arguments).map(function(m) {
-        if ((m && m.stack && m.message) || typeof m === 'object') {
-          msg.push(m);
-        } else if (typeof chalk === 'undefined') {
-          msg.push(`%c${m}`);
-          msg.push('font-size: 14px; color: orange');
-        } else {
-          msg.push(chalk.bold.yellow(m));
-        }
-      });
-    } else {
-      var msg = Array.from(arguments);
-    }
-    console.log.apply(null, msg);
+    var css;
+    var cf;
+    typeof chalk === 'undefined'
+      ? (css = 'font-size: 14px; color: orange; font-weight: bold')
+      : (cf = chalk.bold.yellow);
+    this._msg.apply(this, [css, cf].concat(Array.from(arguments)));
   }
 
   error() {
-    if (this.colored) {
-      var msg = [];
-      Array.from(arguments).map(function(m) {
-        if ((m && m.stack && m.message) || typeof m === 'object') {
-          msg.push(m);
-        } else if (typeof chalk === 'undefined') {
-          msg.push(`%c${m}`);
-          msg.push('font-size: 14px; color: red');
-        } else {
-          msg.push(chalk.bold.red(m));
-        }
-      });
-    } else {
-      var msg = Array.from(arguments);
-    }
-    console.log.apply(null, msg);
+    var css;
+    var cf;
+    typeof chalk === 'undefined'
+      ? (css = 'font-size: 16px; color: red; font-weight: bold')
+      : (cf = chalk.bold.red);
+    this._msg.apply(this, [css, cf].concat(Array.from(arguments)));
   }
 
   critical() {
-    if (this.colored) {
-      var msg = [];
-      Array.from(arguments).map(function(m) {
-        if ((m && m.stack && m.message) || typeof m === 'object') {
-          msg.push(m);
-        } else if (typeof chalk === 'undefined') {
-          msg.push(`%c${m}`);
-          msg.push('font-size: 14px; color: red');
-        } else {
-          msg.push(chalk.bold.red(m));
-        }
-      });
-    } else {
-      var msg = Array.from(arguments);
-    }
-    console.log.apply(null, msg);
+    var css;
+    var cf;
+    typeof chalk === 'undefined'
+      ? (css = 'font-size: 25px; color: red; font-weight: bold')
+      : (cf = chalk.bold.red);
+    this._msg.apply(this, [css, cf].concat(Array.from(arguments)));
   }
 }
 
