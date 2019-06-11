dd6801f1021f8486df0af7b12af18d6f9b806f76
divisor <div@altertech.com>
2019-06-11 03:38:15 +0200

    cookie fix

--- src/@altertech/cookies/index.js
+++ src/@altertech/cookies/index.js
@@ -16,12 +16,12 @@ function create_cookie(name, value, days, path) {
 }
 
 function read_cookie(name) {
-  if (typeof document !== 'undefined') {
-    var nameEQ = name + '=';
-    document.cookie.split(';').map(function(c) {
-      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    });
+  var nameEQ = name + '=';
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
   }
   return null;
 }
