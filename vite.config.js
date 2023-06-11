c9d3e2fd6fddd793a83f14b1567b99690250383b
Serhij S <div@altertech.com>
2023-06-11 16:48:18 +0200

    tsd

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-const path = require('path')
-const { defineConfig } = require('vite')
+import * as path from 'path';
+import { defineConfig } from "vite";
 
-const lib_name = 'jsaltt';
+const lib_name = "jsaltt";
 
 module.exports = defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/lib.ts'),
+      entry: path.resolve(__dirname, "src/lib.ts"),
       name: lib_name,
       fileName: (format) => `${lib_name}.${format}.js`
     }
