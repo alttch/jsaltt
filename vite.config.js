a0ab6fca711f2bfd76a532b209948404554ad907
Serhij S <div@altertech.com>
2023-06-12 02:51:55 +0200

    0.1.8

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ const lib_name = "jsaltt";
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/lib.ts"),
+      entry: path.resolve(__dirname, "src/lib.mts"),
       name: lib_name,
       fileName: (format) => `${lib_name}.${format}.js`
     }
