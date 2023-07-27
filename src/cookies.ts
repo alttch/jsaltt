8b72d3532bb546913cf794109bd6736f7937199e
Serhij S <div@altertech.com>
2023-07-27 17:17:31 +0200

    var -> let

--- src/cookies.ts
+++ src/cookies.ts
@@ -1,10 +1,10 @@
 class Cookies {
   create(name: string, value: any, days?: number, path?: string) {
     if (typeof document === "undefined") return undefined;
-    var p = path !== undefined ? path : "";
-    var expires;
+    let p = path !== undefined ? path : "";
+    let expires;
     if (days) {
-      var date = new Date();
+      let date = new Date();
       date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
       expires = date.toUTCString();
     } else {
@@ -15,10 +15,10 @@ class Cookies {
   }
   read(name: string) {
     if (typeof document === "undefined") return undefined;
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
       while (c.charAt(0) == " ") c = c.substring(1, c.length);
       if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
